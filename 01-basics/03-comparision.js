0e043518c4a44607a078ca226b11d669ac9a63b7
Naitik-Kukadiya <160041102+Naitik-Kukadiya@users.noreply.github.com>
2024-03-06 03:31:08 +0000

    comparision

---
+++ 01-basics/03-comparision.js
@@ -0,0 +1,22 @@
+//console.log(1 < 2);
+//console.log(1 > 2);
+//console.log(1 >= 2);
+//console.log(1 <= 1);
+//console.log(1 == 2);
+
+// console.log("1" > 2);
+// console.log("1" >= 2);
+// console.log("1" == 1);
+// console.log("1" === 2);
+
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null > 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined => 0);
+// //console.log(undefined =< 0);
+// console.log(undefined >= 0);
+// console.log(undefined === 0);
