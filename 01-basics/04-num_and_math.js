61b6126d3fe8a3980d09da557f5161cafc2c264e
Naitik-Kukadiya <160041102+Naitik-Kukadiya@users.noreply.github.com>
2024-03-24 07:46:54 +0000

    math or num

---
+++ 01-basics/04-num_and_math.js
@@ -0,0 +1,31 @@
+const num = 45;
+console.log(num);
+
+const score = Number(100.);
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+
+const otherNumber = 264.454545
+console.log(otherNumber.toPrecision(5));
+
+const bigNum = 1000000000000;
+console.log(bigNum.toLocaleString());
+
+
+
+
+//++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-45));
+// console.log(Math.round(552.46));
+// console.log(Math.ceil(45.2));
+// console.log(Math.floor(45.11));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(7, 45, 18, 33));
+// console.log(Math.max(7, 45, 18, 33));
+console.log(Math.random());
+console.log(Math.random() + 1);
+console.log((Math.random()*10) + 1);
