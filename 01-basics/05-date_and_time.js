ac9a1e34141653626fe9fb05f0e36423dadbd3c7
Naitik-Kukadiya <160041102+Naitik-Kukadiya@users.noreply.github.com>
2024-03-24 11:24:44 +0000

    date and time

---
+++ 01-basics/05-date_and_time.js
@@ -0,0 +1,15 @@
+const dateTest = new Date();
+// console.log(dateTest);
+// console.log(dateTest.toDateString());
+// console.log(dateTest.toISOString());
+// console.log(dateTest.toLocaleDateString());
+// console.log(dateTest.toLocaleString());
+// console.log(dateTest.toLocaleTimeString());
+// console.log(dateTest.toString());
+// console.log(dateTest.toTimeString());
+
+let myCreatDate = new Date(2024, 5, 3);
+
+console.log(myCreatDate.toDateString());
+
+
