0dd760c77c9f712f0d52e4444ee7933b5b33f906
Naitik-Kukadiya <160041102+Naitik-Kukadiya@users.noreply.github.com>
2024-03-26 06:31:25 +0000

    array

---
+++ 02-basics/01-array.js
@@ -0,0 +1,22 @@
+let myArr = [1, 2, 3, 4, 5];
+
+let myHeros = ["rohit", 45, 264];
+
+let myArr2 = new Array('rohit', 'kohli', 'dhoni');
+
+
+//+++++++++++++++++++++ Array ++++++++++++++++++++++++++++
+myArr.push(6)
+myArr.pop(6)
+
+myArr.unshift(10)
+myArr.shift(10)
+
+
+// console.log(myHeros.includes('rohit'));
+// console.log(myHeros.indexOf('rohit'));
+
+
+let newArr = myArr.join()
+// console.log(typeof newArr);
+
